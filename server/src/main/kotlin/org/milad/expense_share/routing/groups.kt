f48d2076184c61e6832876c1cfc764322d8f321f
Milad Targholi <targholi2112@gmail.com>
2025-09-10 19:00:51 +0330

    Add transaction routes
    - Create a new transaction
    - Get list of transactions for a group
    - Approve a transaction
    - Reject a transaction
    - Delete a transaction

--- server/src/main/kotlin/org/milad/expense_share/routing/groups.kt
+++ server/src/main/kotlin/org/milad/expense_share/routing/groups.kt
@@ -14,6 +14,7 @@ import io.ktor.server.routing.route
 import org.milad.expense_share.database.FakeDatabase
 import org.milad.expense_share.model.AddUserRequest
 import org.milad.expense_share.model.CreateGroupRequest
+import org.milad.expense_share.model.CreateTransactionRequest
 
 internal fun Routing.groupsRoutes() {
     route("/groups") {
@@ -63,6 +64,88 @@ internal fun Routing.groupsRoutes() {
                     call.respond(HttpStatusCode.OK, "Group deleted successfully")
                 } else {
                     call.respond(HttpStatusCode.Forbidden, "Only the owner can delete the group")
+                }
+            }
+
+            route("/{groupId}/transactions") {
+                post {
+                    val principal = call.principal<JWTPrincipal>()
+                    val userId = principal!!.payload.getClaim("id").asInt()
+                    val groupId = call.parameters["groupId"]?.toIntOrNull()
+                    val req = call.receive<CreateTransactionRequest>()
+                    if (groupId == null) {
+                        call.respond(HttpStatusCode.BadRequest, "Invalid groupId")
+                    } else {
+                        val tx = FakeDatabase.createTransaction(
+                            groupId,
+                            userId,
+                            req.title,
+                            req.amount,
+                            req.description
+                        )
+                        if (tx != null) call.respond(tx) else call.respond(
+                            HttpStatusCode.BadRequest,
+                            "Group not found"
+                        )
+                    }
+                }
+
+                get {
+                    val principal = call.principal<JWTPrincipal>()
+                    val userId = principal!!.payload.getClaim("id").asInt()
+                    val groupId = call.parameters["groupId"]?.toIntOrNull()
+
+                    if (groupId == null) {
+                        call.respond(HttpStatusCode.BadRequest, "Invalid groupId")
+                    } else {
+                        val txs = FakeDatabase.getTransactions(userId,groupId)
+                        call.respond(txs)
+                    }
+                }
+
+                post("/{transactionId}/approve") {
+                    val principal = call.principal<JWTPrincipal>()
+                    val managerId = principal!!.payload.getClaim("id").asInt()
+                    val transactionId = call.parameters["transactionId"]?.toIntOrNull()
+                    if (transactionId == null) {
+                        call.respond(HttpStatusCode.BadRequest, "Invalid transactionId")
+                    } else {
+                        val success = FakeDatabase.approveTransaction(transactionId, managerId)
+                        if (success) call.respond(
+                            HttpStatusCode.OK,
+                            "Transaction approved"
+                        ) else call.respond(HttpStatusCode.Forbidden, "Only owner can approve")
+                    }
+                }
+
+                post("/{transactionId}/reject") {
+                    val principal = call.principal<JWTPrincipal>()
+                    val managerId = principal!!.payload.getClaim("id").asInt()
+                    val transactionId = call.parameters["transactionId"]?.toIntOrNull()
+                    if (transactionId == null) {
+                        call.respond(HttpStatusCode.BadRequest, "Invalid transactionId")
+                    } else {
+                        val success = FakeDatabase.rejectTransaction(transactionId, managerId)
+                        if (success) call.respond(
+                            HttpStatusCode.OK,
+                            "Transaction rejected"
+                        ) else call.respond(HttpStatusCode.Forbidden, "Only owner can reject")
+                    }
+                }
+
+                delete("/{transactionId}") {
+                    val principal = call.principal<JWTPrincipal>()
+                    val managerId = principal!!.payload.getClaim("id").asInt()
+                    val transactionId = call.parameters["transactionId"]?.toIntOrNull()
+                    if (transactionId == null) {
+                        call.respond(HttpStatusCode.BadRequest, "Invalid transactionId")
+                    } else {
+                        val success = FakeDatabase.deleteTransaction(transactionId, managerId)
+                        if (success) call.respond(
+                            HttpStatusCode.OK,
+                            "Transaction deleted"
+                        ) else call.respond(HttpStatusCode.Forbidden, "Only owner can delete")
+                    }
                 }
             }
         }
