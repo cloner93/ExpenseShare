13b1f0f2afb68aff7e825dea78cff4a537aed080
milad targholi <targholi2112@gmail.com>
2025-11-05 21:09:34 +0330

    Refactor: add koin setup to main of platforms.

---
+++ composeApp/src/androidMain/kotlin/org/milad/expense_share/ExpenseShareApp.kt
@@ -0,0 +1,19 @@
+package org.milad.expense_share
+
+import android.app.Application
+import org.koin.android.ext.koin.androidContext
+import org.koin.android.ext.koin.androidLogger
+import org.koin.core.context.GlobalContext.startKoin
+import org.koin.core.logger.Level
+import org.milad.expense_share.di.appModules
+
+class ExpenseShareApp : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        startKoin {
+            androidContext(this@ExpenseShareApp)
+            androidLogger(Level.DEBUG)
+            modules(appModules)
+        }
+    }
+}
--- composeApp/src/jvmMain/kotlin/org/milad/expense_share/main.kt
+++ composeApp/src/jvmMain/kotlin/org/milad/expense_share/main.kt
@@ -2,8 +2,15 @@ package org.milad.expense_share
 
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
+import org.koin.core.context.GlobalContext.startKoin
+import org.koin.core.logger.Level
+import org.milad.expense_share.di.appModules
 
 fun main() = application {
+    startKoin {
+        printLogger(Level.INFO)
+        modules(appModules)
+    }
     Window(
         onCloseRequest = ::exitApplication,
         title = "ExpenseShare",
