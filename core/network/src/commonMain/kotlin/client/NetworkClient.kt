21c74dcf149c1e215bc77344e7516700fe384af8
milad targholi <targholi2112@gmail.com>
2025-11-08 13:28:59 +0330

    Feat: change default server port and client baseUrl from 8080 to 8082.

--- core/network/src/commonMain/kotlin/client/NetworkClient.kt
+++ core/network/src/commonMain/kotlin/client/NetworkClient.kt
@@ -6,14 +6,12 @@ import io.ktor.client.engine.HttpClientEngine
 import io.ktor.client.plugins.HttpTimeout
 import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
 import io.ktor.client.plugins.defaultRequest
-import io.ktor.client.plugins.logging.DEFAULT
 import io.ktor.client.plugins.logging.LogLevel
 import io.ktor.client.plugins.logging.Logger
 import io.ktor.client.plugins.logging.Logging
 import io.ktor.client.request.header
 import io.ktor.http.ContentType
 import io.ktor.http.HttpHeaders
-import io.ktor.http.URLProtocol
 import io.ktor.http.contentType
 import io.ktor.serialization.kotlinx.json.json
 import kotlinx.serialization.json.Json
@@ -21,7 +19,7 @@ import plugin.installErrorHandler
 import token.TokenProvider
 
 data class HttpConfig(
-    val baseUrl: String = "http://localhost:8080",
+    val baseUrl: String = "http://localhost:8082",
     val timeoutMillis: Long = 15000,
     val isDebug: Boolean = true,
     val refreshTokenEndpoint: String = "/auth/refresh" // FIXME
