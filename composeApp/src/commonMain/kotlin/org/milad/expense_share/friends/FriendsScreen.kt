b90b68ecc995d13ff2e42f5557696fe558059f74
milad targholi <targholi2112@gmail.com>
2025-12-14 10:26:46 +0330

    style(ui): Add placeholder content for Friends and Profile screens

--- composeApp/src/commonMain/kotlin/org/milad/expense_share/friends/FriendsScreen.kt
+++ composeApp/src/commonMain/kotlin/org/milad/expense_share/friends/FriendsScreen.kt
@@ -1,13 +1,22 @@
 package org.milad.expense_share.friends
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.material3.adaptive.ExperimentalMaterial3AdaptiveApi
 import androidx.compose.material3.adaptive.layout.ListDetailPaneScaffold
 import androidx.compose.material3.adaptive.navigation.rememberListDetailPaneScaffoldNavigator
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import org.jetbrains.compose.ui.tooling.preview.Preview
 
 @OptIn(ExperimentalMaterial3AdaptiveApi::class)
 @Composable
@@ -24,10 +33,29 @@ fun FriendsScreen(
         directive = navigator.scaffoldDirective,
         value = navigator.scaffoldValue,
         listPane = {
-
+            Column(
+                modifier = Modifier
+                    .fillMaxSize(),
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally,
+            ) {
+                Text(
+                    modifier = Modifier.padding(8.dp),
+                    text = "Screen under construction",
+                    style = MaterialTheme.typography.titleLarge,
+                    textAlign = TextAlign.Center,
+                    color = MaterialTheme.colorScheme.primary,
+                )
+            }
         },
         detailPane = {
 
         }
     )
+}
+
+@Preview
+@Composable
+fun FriendsScreenPreview() {
+    FriendsScreen()
 }
--- composeApp/src/commonMain/kotlin/org/milad/expense_share/profile/ProfileScreen.kt
+++ composeApp/src/commonMain/kotlin/org/milad/expense_share/profile/ProfileScreen.kt
@@ -1,17 +1,21 @@
 package org.milad.expense_share.profile
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import org.jetbrains.compose.ui.tooling.preview.Preview
 
 @Composable
 fun ProfileScreen(
@@ -23,13 +27,17 @@ fun ProfileScreen(
             modifier = Modifier
                 .fillMaxSize()
                 .padding(paddingValues),
-            horizontalAlignment = Alignment.CenterHorizontally
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally,
         ) {
-            Text("Profile & Settings")
+            Text(
+                modifier = Modifier.padding(8.dp),
+                text = "Screen under construction",
+                style = MaterialTheme.typography.titleLarge,
+                textAlign = TextAlign.Center,
+                color = MaterialTheme.colorScheme.primary,
+            )
             Spacer(Modifier.height(16.dp))
-
-
-            Spacer(Modifier.height(32.dp))
 
             Button(
                 onClick = {
@@ -41,4 +49,10 @@ fun ProfileScreen(
         }
     }
 
+}
+
+@Preview
+@Composable
+fun ProfileScreenPreview() {
+    ProfileScreen(onLogout = {})
 }
