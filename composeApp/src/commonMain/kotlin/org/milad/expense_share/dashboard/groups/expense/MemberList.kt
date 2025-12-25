bca8800c7774602c326543ec29c3fbb4ca05df27
Milad Targholi <targholi2112@gmail.com>
2025-12-25 20:09:45 +0330

    feat(group): Add member list view
    Introduces the `MemberList` Composable to display a list of group members.
    
    - Creates `MemberList` to render a `LazyColumn` of members.
    - Includes an `EmptyListState` for when there are no members.
    - Adds a `MemberRow` Composable to display individual member information, including their name and phone number.
    - Implements logic to show a delete button for each member if the current user is the group owner.
    - The group owner cannot be deleted from the group.

---
+++ composeApp/src/commonMain/kotlin/org/milad/expense_share/dashboard/groups/expense/MemberList.kt
@@ -0,0 +1,114 @@
+package org.milad.expense_share.dashboard.groups.expense
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.DeleteOutline
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.unit.dp
+import com.pmb.common.ui.emptyState.EmptyListState
+import expenseshare.composeapp.generated.resources.Res
+import expenseshare.composeapp.generated.resources.paris
+import model.Group
+import model.User
+import org.jetbrains.compose.resources.painterResource
+
+@Composable
+fun MemberList(
+    members: List<User>,
+    currentUser: User?,
+    selectedGroup: Group,
+    onDeleteClick: (User) -> Unit
+) {
+
+    if (members.isNotEmpty()) {
+        LazyColumn(
+            contentPadding = PaddingValues(horizontal = 16.dp, vertical = 8.dp)
+        ) {
+            items(members) { item ->
+                MemberRow(
+                    item,
+                    isGroupOwner = selectedGroup.ownerId == item.id,
+                    canDeleteUser = currentUser?.id == selectedGroup.ownerId,
+                    onDeleteClick = { onDeleteClick(item) }
+                )
+            }
+        }
+    } else {
+        EmptyListState()
+    }
+}
+
+@Composable
+private fun MemberRow(
+    user: User,
+    isGroupOwner: Boolean = false,
+    canDeleteUser: Boolean = false,
+    onDeleteClick: () -> Unit,
+) {
+    Card(
+        modifier = Modifier
+            .fillMaxWidth()
+            .clip(CardDefaults.shape)
+            .padding(bottom = 4.dp),
+    ) {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(6.dp),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Image(
+                painter = painterResource(Res.drawable.paris),
+                contentDescription = null,
+                modifier = Modifier
+                    .size(40.dp)
+                    .clip(CardDefaults.shape),
+                contentScale = ContentScale.Crop
+            )
+
+            Column(
+                modifier = Modifier
+                    .padding(horizontal = 8.dp)
+                    .weight(1f)
+            ) {
+                Text(
+                    text = user.username,
+                    style = MaterialTheme.typography.bodyLarge.copy(color = MaterialTheme.colorScheme.onSurface)
+                )
+                Spacer(modifier = Modifier.height(4.dp))
+                Text(
+                    text = user.phone,
+                    style = MaterialTheme.typography.bodyMedium.copy(color = MaterialTheme.colorScheme.onSurfaceVariant)
+                )
+            }
+            if (canDeleteUser) {
+                IconButton(onClick = onDeleteClick, enabled = !isGroupOwner) {
+                    Icon(
+                        imageVector = Icons.Default.DeleteOutline,
+                        contentDescription = null,
+                    )
+                }
+            }
+        }
+    }
+}
