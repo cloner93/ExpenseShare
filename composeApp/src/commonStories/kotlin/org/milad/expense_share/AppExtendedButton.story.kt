870915c578929474bd2cbb483d2b5963743b13b6
milad targholi <targholi2112@gmail.com>
2026-02-09 19:03:53 +0330

    feat: Standardize story name for AppExtendedButton

--- composeApp/src/commonStories/kotlin/org/milad/expense_share/AppExtendedButton.story.kt
+++ composeApp/src/commonStories/kotlin/org/milad/expense_share/AppExtendedButton.story.kt
@@ -3,7 +3,7 @@ package org.milad.expense_share
 import org.jetbrains.compose.storytale.story
 import org.milad.expense_share.dashboard.AppExtendedButton
 
-val `AppExtendedButton default state` by story {
+val AppExtendedButtonStory by story {
    val title by parameter("add member")
     AppExtendedButton(title = title , onClick = {})
 }
